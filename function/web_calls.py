6f8112e1db95737855f9d94aac7afda29b795d63
Abdullah <abdullah.binhasan@outlook.com>
2022-01-08 18:06:00 +0600

    added whatsapp for web functionality

--- function/web_calls.py
+++ function/web_calls.py
@@ -67,14 +67,21 @@ def take_user_input():
 	return query
 	
 
+
 def search_on_wikipedia(query):
     results = wikipedia.summary(query, sentences=2)
     return results
+
 def find_my_ip():
     ip_address = requests.get('https://api64.ipify.org?format=json').json()
     return ip_address["ip"]
+
 def play_on_youtube(video):
     kit.playonyt(video)
 
 def search_on_google(query):
     kit.search(query)
+
+def send_whatsapp_message(number, message):
+    kit.sendwhatmsg_instantly(f"+88{number}", message)
+
