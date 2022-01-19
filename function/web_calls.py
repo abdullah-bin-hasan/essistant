a7e87aa072bb9a0a09000f597bfe527f0f7b2cd0
Abdullah <abdullah.binhasan@outlook.com>
2022-01-19 21:58:20 +0600

    added email function

--- function/web_calls.py
+++ function/web_calls.py
@@ -96,3 +96,24 @@ def get_latest_news():
     for article in articles:
         news_headlines.append(article["title"])
     return news_headlines[:5]
+
+EMAIL = config("EMAIL")
+PASSWORD = config("PASSWORD")
+
+
+def send_email(receiver_address, subject, message):
+    try:
+        email = EmailMessage()
+        email['To'] = receiver_address
+        email["Subject"] = subject
+        email['From'] = EMAIL
+        email.set_content(message)
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.send_message(email)
+        s.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
